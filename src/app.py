7f9329572d4026c47ff41a6baac2ee92092d5059
Astrid Mata <86080960+AstridMata@users.noreply.github.com>
2026-05-05 03:06:43 +0000

    get, get_id, post

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planets
 #from models import Person
 
 app = Flask(__name__)
@@ -36,15 +36,109 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+# /////////////////////////////////////////////////////////////////////// trae todos los registros de usuarios
 @app.route('/user', methods=['GET'])
 def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    try:
+        # consultar a la base de datos los registros de usuarios
+        query_results= User.query.all()
 
-    return jsonify(response_body), 200
+        # validar si la lista esta vacia
+        if not query_results:
+            return jsonify({"msg": "Usuarios no encontrados"}), 400 
 
+        # aplico map para usar serialize()
+        results= list(map(lambda item: item.serialize(), query_results))
+
+
+        response_body = {
+            "msg": "lista de usuarios encontrada",
+            "results": results
+        }
+
+        return jsonify(response_body), 200
+           
+    except Exception as error:
+         #en caso de error se captura la excepcion
+        print(f"Error al obtener los ususarios: {error}")
+        return jsonify({"msg": "Internal Server Error", "error": str(error)}), 500
+
+
+
+
+# ///////////////////////////////////////////////////////////////////////// Muestra la información de un solo planeta según su id
+
+
+@app.route('/planet/<int:planet_id>', methods=['GET'])
+def planet_by_id(planet_id):
+
+    try:
+        # consultar en la tabla planets el primer registro que coincid con el id 
+        query_results= Planets.query.filter_by(id=planet_id).first()
+
+        # validar si existe el planeta
+        if not query_results:
+            return jsonify({"msg": "Planeta no encontrado "}), 400 
+
+        response_body = {
+            "msg": "planeta encontrado",
+            "results": query_results.serialize()
+        }
+
+        return jsonify(response_body), 200
+           
+    except Exception as error:
+         #en caso de error se captura la excepcion
+        print(f"Error al obtener el planeta por id: {error}")
+        return jsonify({"msg": "Internal Server Error", "error": str(error)}), 500
+
+
+#////////////////////////////////////////////////////////////////////////////// crea la información de un usuario nuevo
+
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    # siempre en formato JSON
+    data= request.get_json()
+
+    #verificando que el mensaje no este vacio
+    if not data:
+        return jsonify({"msg": "no se proporcionaron datos"}), 400
+
+    #extraer los valores de los campos 
+    email= data.get("email")
+    password= data.get("password")
+    is_active= data.get("is_active", False)
+
+    #validar si el email esta registrado
+    existing_user = User.query.filter_by(email=email).first()
+    if existing_user:
+        return jsonify({"msg": "ya existe un usuario registrado con ese email"}), 409
+    
+    #crea un registro nuevo 
+    new_user= User(
+        email= email,
+        password= password,
+        is_active= is_active
+    )
+
+    #debe ser guardada en la base de datos
+    db.session.add(new_user)
+
+    try:
+        #confirmar los cambios de forma permanente
+        db.session.commit()
+        return jsonify(new_user.serialize()), 201
+    
+    except Exception as error:
+         #en caso de error se captura la excepcion
+        print(f"Error al crear usuario: {error}")
+        return jsonify({"msg": "Internal Server Error", "error": str(error)}), 500
+
+
+#////////////////////////////////////////////////////////////////////////////
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
@@ -17,3 +17,19 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Planets(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=False, nullable=False)
+    climate: Mapped[str] = mapped_column(String(120), nullable=False)
+    diameter: Mapped[int] = mapped_column(Integer, nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "diameter": self.diameter,
+        }
+    
